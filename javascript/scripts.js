4d40c191b619425a378ef09e7a90c2ec2be5bda8
Aiden M <aiden.morrison2005@gmail.com>
2022-12-11 16:23:03 -0700

    kinda works

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -1,3 +1,5 @@
+//navbar
+
 const header = document.querySelector('.navbar');
 
 window.onscroll = function() {
@@ -8,4 +10,35 @@ window.onscroll = function() {
     else {
         header.classList.remove('navbarDark');
     }
+}
+
+//frc image coursel
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+// Next/previous controls
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+// Thumbnail image controls
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";
+  dots[slideIndex-1].className += " active";
 }
