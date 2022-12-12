82c334e85f5962795187c529ff361ab4b66eec34
Aiden Morrison <aiden.morrison2005@gmail.com>
2022-12-11 21:10:42 -0700

    done with website
    done for class assignment

--- javascript/scripts.js
+++ javascript/scripts.js
@@ -27,3 +27,18 @@ function SlideShow() {
   slides[slidePosition-1].style.display = "block";
   setTimeout(SlideShow, 2000); // Change image every 2 seconds
 } 
+// scroll to top functionality
+const scrollUp = document.querySelector("#scroll-up");
+
+scrollUp.addEventListener("click", () => {
+  window.scrollTo({
+    top: 0,
+    left: 0,
+    behavior: "smooth",
+  });
+});
+// Nav hamburgerburger selections
+
+const burger = document.querySelector("#burger-menu");
+const ul = document.querySelector("nav ul");
+const nav = document.querySelector("nav");
